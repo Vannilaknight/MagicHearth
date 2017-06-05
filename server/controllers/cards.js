84b7c4255b41bc47f12bb13eb421b746ce1d7f60
Taylor Boyd <taylorboyd@Taytays-Mac.local>
2017-06-05 12:00:55 -0600

    fixed a bug with the type selection

--- server/controllers/cards.js
+++ server/controllers/cards.js
@@ -58,17 +58,19 @@ function filterFormat(format, cards) {
 }
 
 function filterType(type, cards) {
-    cards = cards.filter(function (card) {
-        var result = false;
-        if(card.types) {
-            card.types.forEach(function (cardType) {
-                if(type.toLowerCase() == cardType.toLowerCase()){
-                    result = true;
-                }
-            })
-        }
-        return result;
-    });
+    if (type != 'none') {
+        cards = cards.filter(function (card) {
+            var result = false;
+            if(card.types) {
+                card.types.forEach(function (cardType) {
+                    if(type.toLowerCase() == cardType.toLowerCase()){
+                        result = true;
+                    }
+                })
+            }
+            return result;
+        });
+    }
     return cards;
 }
 
